2f568fe0f01c017b1959d3d99ce3ea51db1bbc1c
Yashesh Akbari <159016243+yashesh-akbari@users.noreply.github.com>
2024-06-17 17:54:22 +0530

    Add files via upload

---
+++ arithmatic and variable.js
@@ -0,0 +1,8 @@
+/*this is the rafe program pratices for understanding of javascript */
+//console.log(3+2);
+//console.log(3-2);
+//console.log(3*2);
+//console.log(3/2);
+let number = 5;
+let number2 = 10;
+console.log(number+number2);
---
+++ index.js
@@ -0,0 +1,4 @@
+function fun(){
+    document.getElementById('first').innerText ="the text are change";
+    document.getElementsByName('h1')[0].style.color="red";
+}
