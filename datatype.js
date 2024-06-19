cfc19b4b3c1d7763f2a26664a115843f34753979
Yashesh Akbari <159016243+yashesh-akbari@users.noreply.github.com>
2024-06-19 20:21:08 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,26 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4) 
