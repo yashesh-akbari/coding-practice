2fae8f5a596b1c7bb2749c6ffec85903512dd4cb
Yashesh Akbari <159016243+yashesh-akbari@users.noreply.github.com>
2024-08-01 07:19:40 +0530

    basic web using include

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php
+// include() = cpoies the context of a file (php/html/text) and includes it in your php file. section of our website become reusable changes only need to be made in one place
+include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>this is About page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, consequuntur dolorem dolorum molestias minus, illum fugit unde nostrum repellat sapiente voluptates mollitia corrupti reiciendis sint facilis quisquam quam sed, totam perferendis beatae porro autem alias consequatur tenetur! Illum maxime natus obcaecati eum dicta ducimus, sit nostrum fugiat. Dicta, facere corporis.</p>
+  <hr>
+</body>
+
+</html>
+
+<?php
+include("footer.html");
+?>
