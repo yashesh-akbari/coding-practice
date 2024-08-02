88410e5f17b1b49fe2b84c6ca312d6320bfaae5f
Yashesh Akbari <159016243+yashesh-akbari@users.noreply.github.com>
2024-08-02 21:04:15 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <header><button name="logout" value="logout"></button></header>
+  <form action="home.php" method="post">
+    <h1>this is home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque aliquid, eos exercitationem deserunt fuga optio quo autem suscipit beatae praesentium ea maiores officia. Fugiat, consectetur? Itaque minima modi, esse at repellat voluptate, reiciendis sequi deserunt aspernatur adipisci corrupti impedit numquam in assumenda eaque eveniet delectus debitis distinctio quibusdam provident. Repellat.</p>
+  </form>
+</body>
+
+</html>
+<?php
+if (isset($_POST["logout"]));
+header("Location: index.php");
+session_destroy();
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-// include() = cpoies the context of a file (php/html/text) and includes it in your php file. section of our website become reusable changes only need to be made in one place
-include("header.html");
+
+// session = 5GB used to store information on a user to be used across mutiple pages.
+// A user is assigned a session-id ex. login credentials
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,17 +10,35 @@ include("header.html");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <!-- login form example understand the session method -->
+  <title>Login Form</title>
 </head>
 
 <body>
-  <h1>this is landing page</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, consequuntur dolorem dolorum molestias minus, illum fugit unde nostrum repellat sapiente voluptates mollitia corrupti reiciendis sint facilis quisquam quam sed, totam perferendis beatae porro autem alias consequatur tenetur! Illum maxime natus obcaecati eum dicta ducimus, sit nostrum fugiat. Dicta, facere corporis.</p>
-  <hr>
+  <form action="index.php" method="post">
+    username: <input type="text" name="username">
+    password: <input type="password" name="password">
+    <input type="submit" value="login">
+  </form>
 </body>
 
 </html>
-
 <?php
-include("footer.html");
+if (isset($_POST["login"])) {
+  if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
+
+
+    echo "login successful!!!";
+    echo $_SESSION["username"];
+    echo $_SESSION["password"];
+
+    header("Location: home.php");
+  } else {
+    echo "username or password are missing!!!";
+  }
+} else {
+  echo "username or password are missing!!!";
+}
 ?>
